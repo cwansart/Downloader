4a037bf5e82831a968f5a0e844e1b3b2021a5c7d
Christian Wansart <cwansart@users.noreply.github.com>
2016-01-11 13:34:01 +0100

    Fixed loading image issue.

--- src/de/cwansart/LoadingDialog.java
+++ src/de/cwansart/LoadingDialog.java
@@ -13,12 +13,12 @@ import org.apache.commons.io.FileUtils;
 public class LoadingDialog extends JDialog {	
 	private static final long serialVersionUID = 1138442902958974678L;
 
-	public LoadingDialog() {		
-		ImageIcon loadingImage = new ImageIcon("images/loading40.gif");
+	public LoadingDialog() {
+		ImageIcon loadingImage = new ImageIcon(Main.class.getResource("/loading40.gif"));
 		JLabel loadingLabel = new JLabel(loadingImage);
 		this.add(loadingLabel);
 		
-//		this.setModal(true);
+		this.setModal(true);
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.pack();
 		this.setLocationRelativeTo(null);
